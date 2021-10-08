37f3ccc129cd1deb4a007e15446cdd42ba9608bf
Kolya <kolianos007@gmail.com>
2021-10-08 17:51:34 +0300

    rewrite id for notes after delete note from array

--- src/redux/actions/notes.js
+++ src/redux/actions/notes.js
@@ -29,13 +29,40 @@ const getNote = () => async (dispatch) => {
   const uid = localStorage.getItem("localId");
   const authTok = localStorage.getItem("idToken");
   dispatch(notesLoaded());
-  const notesList = await axios
+  const list = await axios
     .get(
       `https://appnoteredux-55ec0-default-rtdb.firebaseio.com/notes/${uid}.json?auth=${authTok}`
     )
     // .then((res) => dispatch(notesSuccess(res.data)))
-    .then((res) => dispatch(notesSuccess(res.data)))
-    .catch((err) => dispatch(notesError(err)));
+    .then((res) => {
+      console.log("results", res.data);
+      const filterNote = res.data
+        .filter((e) => e)
+        .map((el, i, arr) => {
+          console.log(arr);
+          // eslint-disable-next-line no-param-reassign
+          arr[i].notesList = el.notesList.filter((e) => e);
+          return el;
+        });
+      console.log(filterNote);
+      // dispatch(notesSuccess(filterNote));
+      return filterNote;
+    })
+    .catch((err) => {
+      console.log(err);
+      return false;
+    });
+
+  let notesList;
+  if (list) {
+    notesList = await axios
+      .put(
+        `https://appnoteredux-55ec0-default-rtdb.firebaseio.com/notes/${uid}.json?auth=${authTok}`,
+        { ...list }
+      )
+      .then((res) => dispatch(notesSuccess(res.data)))
+      .catch((err) => dispatch(notesError(err)));
+  }
 
   return notesList;
 };
