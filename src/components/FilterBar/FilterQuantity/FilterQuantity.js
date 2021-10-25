a13e9053786e6b26cc9b2bb01eac75a9e9e97666
kolianos007 <kolianos007@gmail.com>
2021-10-25 07:42:44 +0300

    add visibleBlock

--- src/components/FilterBar/FilterQuantity/FilterQuantity.js
+++ src/components/FilterBar/FilterQuantity/FilterQuantity.js
@@ -5,7 +5,7 @@ import s from "../FilterBar.module.sass";
 
 const FilterQuantity = () => {
   const quantityPerPage = [2, 4, 8, 16];
-  const [selected, setSelected] = useState(8);
+  const [selected, setSelected] = useState(2);
   return (
     <div className={s.filterQuantity}>
       <span className={s.filterTitle}>К-во блоков на странице:</span>
--- src/containers/Home/NotesList/NotesList.js
+++ src/containers/Home/NotesList/NotesList.js
@@ -8,19 +8,34 @@ import Loader from "../../../components/Loader";
 import s from "./NotesList.module.sass";
 import BlockDateNotes from "../../../components/BlockDateNotes/BlockDateNotes";
 import { getNote } from "../../../redux/actions/notes";
+import Button from "../../../components/UI/Button";
 
-const NotesList = ({ notes, loader, getNoteConnect }) => {
+const NotesList = ({ notes, loader, getNoteConnect, visibleBlock }) => {
   useEffect(() => {
     getNoteConnect();
   }, []);
+
+  const getMoreNotes = () => {};
 
   return (
     <div className={s.listWrapper}>
       {loader ? (
         <Loader width="5rem" height="5rem" />
       ) : notes ? (
-        notes.map(({ date, id, notesList }) => {
-          return <BlockDateNotes key={id} notesDate={date} notes={notesList} />;
+        notes.map(({ date, id, notesList }, i) => {
+          return visibleBlock > i ? (
+            <BlockDateNotes key={id} notesDate={date} notes={notesList} />
+          ) : (
+            <Button
+              className="btnWrapper btnWrapper_center"
+              buttonClass="btn btn_sm"
+              text="Показать еще"
+              loader
+              loading={loader}
+              loaderSize="3.125rem"
+              onClick={() => getMoreNotes()}
+            />
+          );
         })
       ) : (
         <EmptyList />
@@ -33,6 +48,7 @@ NotesList.propTypes = {
   notes: PropTypes.arrayOf(PropTypes.object),
   loader: PropTypes.bool.isRequired,
   getNoteConnect: PropTypes.func.isRequired,
+  visibleBlock: PropTypes.number.isRequired,
 };
 
 NotesList.defaultProps = {
@@ -43,6 +59,7 @@ const mapStateToProps = ({ notes }) => {
   return {
     notes: notes.notesList,
     loader: notes.loading,
+    visibleBlock: notes.visibleBlock,
   };
 };
 
